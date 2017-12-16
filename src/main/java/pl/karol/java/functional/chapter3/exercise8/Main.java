25b273b83af219067a744aa4ad81b18aef75edf7
kgajda <karoll.gajda@gmail.com>
2017-12-16 15:38:57 +0100

    exercise 3.8

---
+++ src/main/java/pl/karol/java/functional/chapter3/exercise8/Main.java
@@ -0,0 +1,21 @@
+package pl.karol.java.functional.chapter3.exercise8;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(1, 2, 4, 5, 6);
+        Function<Integer, Function<String, String>> function = x -> y -> addSI(x, y);
+        String identity = "0";
+        String sum = CollectionUtilites.foldRightRec(integers, identity, function);
+        System.out.println("Suma: " + sum);
+    }
+
+    private static String addSI(Integer x, String y) {
+        return "(" + x + " + " + y + " )";
+    }
+}
